cd2ebd743ec158617e2bb2546462c5e3a55dae18
Gismat <qismeths@gmail.com>
2022-03-03 16:22:26 +0400

    Running app when connect to database succesfully

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const db = require('./util/database')
+const sequelize = require('./util/database')
 const app = express();
 
 
@@ -15,5 +15,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(errorController.get404);
-
-app.listen(3000);
+sequelize.sync().then(result => {
+    // console.log(result)
+    app.listen(3000);
+}).catch(err => console.log(err))
