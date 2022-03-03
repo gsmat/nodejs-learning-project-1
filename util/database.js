6caf6a0eb3eeb1016eb1c3d617aa52e6a81109bf
Gismat <qismeths@gmail.com>
2022-03-03 16:02:01 +0400

    configuration sequelize to connect database

--- util/database.js
+++ util/database.js
@@ -1,4 +1,13 @@
-const mysql = require('mysql2')
+const {Sequelize} = require('sequelize');
+const sequelize = new Sequelize('node-complete', 'root', '', {
+    dialect: 'mysql',
+    host: 'localhost'
+});
+
+
+module.exports = sequelize;
+
+/*const mysql = require('mysql2')
 
 const pool = mysql.createPool({
     host: '127.0.0.1',
@@ -7,4 +16,4 @@ const pool = mysql.createPool({
     database: 'node-complete'
 })
 
-module.exports = pool.promise();
+module.exports = pool.promise();*/
