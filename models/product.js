435d03dae2cdb69411f550941552cb42c219c430
Gismat <qismeths@gmail.com>
2022-03-03 16:02:27 +0400

    Change to raw sql to ORM and define model

--- models/product.js
+++ models/product.js
@@ -1,82 +1,27 @@
-const fs = require('fs');
-const path = require('path');
-const db = require('../util/database')
-const Cart = require('./cart');
+const Sequelize = require('sequelize');
 
-const p = path.join(
-    path.dirname(process.mainModule.filename),
-    'data',
-    'products.json'
-);
+const sequelize = require('../util/database')
 
-const getProductsFromFile = cb => {
-    fs.readFile(p, (err, fileContent) => {
-        if (err) {
-            cb([]);
-        } else {
-            cb(JSON.parse(fileContent));
-        }
-    });
-};
-
-module.exports = class Product {
-    constructor(id, title, imageUrl, description, price) {
-        this.id = id;
-        this.title = title;
-        this.imageUrl = imageUrl;
-        this.description = description;
-        this.price = price;
+const Product = sequelize.define('product', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    title: Sequelize.STRING,
+    price: {
+        type: Sequelize.DOUBLE,
+        allowNull: false,
+    },
+    imageUrl: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.STRING,
+        allowNull: false
     }
+})
 
-    save() {
-        /*
-        getProductsFromFile(products => {
-          if (this.id) {
-            const existingProductIndex = products.findIndex(
-              prod => prod.id === this.id
-            );
-            const updatedProducts = [...products];
-            updatedProducts[existingProductIndex] = this;
-            fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-              console.log(err);
-            });
-          } else {
-            this.id = Math.random().toString();
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), err => {
-              console.log(err);
-            });
-          }
-        });
-         */
-    }
-
-    static deleteById(id) {
-        return db.execute('delete from products where id =' + id)
-        /*
-        getProductsFromFile(products => {
-          const product = products.find(prod => prod.id === id);
-          const updatedProducts = products.filter(prod => prod.id !== id);
-          fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-            if (!err) {
-              Cart.deleteProduct(id, product.price);
-            }
-          });
-        });
-         */
-    }
-
-    static fetchAll(cb) {
-        return db.execute('select * from products')
-        // getProductsFromFile(cb);
-    }
-
-    static findById(id, cb) {
-        return db.execute('select * from products where id =' + id)
-
-        // getProductsFromFile(products => {
-        //     const product = products.find(p => p.id === id);
-        //     cb(product);
-        // });
-    }
-};
+module.exports = Product;
